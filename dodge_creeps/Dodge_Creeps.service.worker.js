0385621208b15773f4f6b0390157900a0b254bd6
Viswa Mohanty <4842533+vpmohanty@users.noreply.github.com>
2023-01-02 23:01:27 -0500

    Improved mouse tracking in Dodge Creeps

--- dodge_creeps/Dodge_Creeps.service.worker.js
+++ dodge_creeps/Dodge_Creeps.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1672632698|17089084977";
+const CACHE_VERSION = "1672718393|102787405971";
 const CACHE_PREFIX = "Dodge_Creeps-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "Dodge_Creeps.offline.html";
